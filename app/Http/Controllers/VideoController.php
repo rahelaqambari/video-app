9736ef2eb8a35265d7e1e2c2dd76913521e9ab3d
rahelaqambari <rahelaqambari@gmail.com>
2026-04-21 22:17:54 +0430

    changes

---
+++ app/Http/Controllers/VideoController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Video;
+use Illuminate\Http\Request;
+
+class VideoController extends Controller
+{
+    //
+    public function index(){
+
+    }
+
+    public function show(string $id){
+
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'video' => 'required|file|mimes:mp4,mov,avi,|max:51200',
+            'title' => 'required'
+        ]);
+        $path = $request->file('video')->store('video','public');
+
+        Video::create([
+            'title' => $request->title,
+            'video_path' => $path,
+            // 'user_id' => auth()->id(),
+        ]);
+        return 
+        response()->json('data inserted');
+    }
+}
