260d23ea3ef08f1bdf6ddd14cb582718b0fd372c
rahelaqambari <rahelaqambari@gmail.com>
2026-05-05 20:48:52 +0430

    changes

--- app/Models/VideoVeiws.php
+++ app/Models/VideoVeiws.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class VideoVeiws extends Model
 {
     //
+    protected $fillable = [
+        'user_id',
+        'video_id'
+    ];
 }
--- resources/views/components/all-video.blade.php
+++ resources/views/components/all-video.blade.php
@@ -1,3 +1,5 @@
 <div>
     <!-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk -->
+    <h1>My Jornuy About this Title</h1>
+    <h1>lorem empoesou doler site amed /h1>
 </div>
